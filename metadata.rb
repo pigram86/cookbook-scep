f880d4dd6df4edd83518d68e3a9c53a6720e717d
pigram86 <todd@toddpigram.com>
2014-02-10 08:01:05 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'scep'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures scep'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.8.0'
+depends          "windows"
+
